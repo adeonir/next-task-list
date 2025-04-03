b1b66a6cd3179c74c66593892cc18144e8780a0b
Adeonir Kohl <adeonir@gmail.com>
2025-04-03 07:54:00 -0300

    Create button ui component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,52 @@
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+import { ComponentProps } from "react"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  [
+    "inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap rounded-md font-medium text-sm outline-none",
+    "transition-all focus-visible:border-gray-950 focus-visible:ring-[3px] focus-visible:ring-gray-950/50 disabled:pointer-events-none",
+    "disabled:opacity-50 aria-invalid:border-red-500 aria-invalid:ring-red-500/20 [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+  ],
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-900 text-gray-50 shadow-xs hover:bg-gray-900/90",
+        destructive: "bg-red-500 text-white shadow-xs hover:bg-red-500/90 focus-visible:ring-red-500/20",
+        outline: "border bg-white shadow-xs hover:bg-gray-100 hover:text-gray-900",
+        secondary: "bg-gray-100 text-gray-900 shadow-xs hover:bg-gray-100/80",
+        ghost: "hover:bg-gray-100 hover:text-gray-900",
+        link: "text-gray-900 underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+)
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : "button"
+
+  return <Comp className={cn(buttonVariants({ variant, size, className }))} data-slot="button" {...props} />
+}
+
+export { Button, buttonVariants }
