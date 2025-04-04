9f80a3e7fe9f6250a86e77fc557bfc5e1c95d792
Adeonir Kohl <adeonir@gmail.com>
2025-04-04 08:54:55 -0300

    Fix navigation-menu focus styles

--- src/components/app/navigation-menu.tsx
+++ src/components/app/navigation-menu.tsx
@@ -24,7 +24,7 @@ export function AppNavigationMenu({ items }: NavigationMenuProps) {
               className={cn(
                 "inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 font-medium text-sm outline-none transition-all",
                 "hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus-visible:outline-1",
-                "focus-visible:ring-[3px] focus-visible:ring-gray-950/50 disabled:pointer-events-none disabled:opacity-50",
+                "focus-visible:ring-[1px] focus-visible:ring-gray-950/50 disabled:pointer-events-none disabled:opacity-50",
                 "data-[active=true]:bg-gray-100 data-[active=true]:text-gray-900 data-[active=true]:focus:bg-gray-100 data-[active=true]:hover:bg-gray-100",
               )}
               data-active={pathname === item.href}
