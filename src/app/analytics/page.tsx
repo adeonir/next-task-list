66207f440e3dddf13515304f785b42e4f700d757
Adeonir Kohl <adeonir@gmail.com>
2025-04-03 21:40:38 -0300

    Create dashboard and analytics pages with header

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,6 @@
+import { ChartColumnBig } from "lucide-react"
+import { AppHeader } from "@/components/app/header"
+
+export default function Dashboard() {
+  return <AppHeader icon={ChartColumnBig} title="Analytics" />
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { CircleCheckBig } from "lucide-react"
+import { AppHeader } from "@/components/app/header"
+
+export default function Dashboard() {
+  return <AppHeader icon={CircleCheckBig} title="Task List" />
+}
