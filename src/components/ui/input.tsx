12deaaa43dd7e9b0eeec3e6ea28de0cde87362cb
Adeonir Kohl <adeonir@gmail.com>
2025-04-04 22:22:59 -0300

    Update styles on input and textarea components

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
     <input
       className={cn(
         "flex h-9 w-full min-w-0 rounded-md border border-gray-200 bg-transparent px-3 py-1 text-base shadow-xs outline-none",
-        "transition-[color,box-shadow] selection:bg-gray-900 selection:text-gray-50 file:inline-flex file:h-7 file:border-0",
+        "transition-[color,box-shadow] selection:bg-blue-100 selection:text-gray-950 file:inline-flex file:h-7 file:border-0",
         "file:bg-transparent file:font-medium file:text-gray-950 file:text-sm placeholder:text-gray-500",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-gray-950 focus-visible:ring-[3px] focus-visible:ring-blue-500/20",
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ function Textarea({ className, ...props }: ComponentProps<"textarea">) {
         "field-sizing-content flex min-h-16 w-full rounded-md border border-gray-200 bg-transparent px-3 py-2 text-base shadow-xs",
         "outline-none transition-[color,box-shadow] placeholder:text-gray-500 focus-visible:border-gray-950 focus-visible:ring-[3px]",
         "focus-visible:ring-blue-500/20 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-red-500",
-        "aria-invalid:ring-red-500/20 md:text-sm",
+        "selection:bg-blue-100 selection:text-gray-950 aria-invalid:ring-red-500/20 md:text-sm",
         className,
       )}
       data-slot="textarea"
