e656176800ea067f8e5b24eaca4b2cdfa1881ab9
Adeonir Kohl <adeonir@gmail.com>
2025-04-04 22:49:47 -0300

    Rename task schema

--- src/schemas/task.ts
+++ src/schemas/task.ts
@@ -1,11 +1,11 @@
 import { Priority, Status } from "@prisma/client"
 import { z } from "zod"
 
-export const createTaskSchema = z.object({
+export const taskSchema = z.object({
   title: z.string().trim().min(1, { message: "Título é obrigatório" }),
   description: z.string().trim().min(1, { message: "Descrição é obrigatória" }),
   status: z.nativeEnum(Status, { message: "Status é obrigatório" }),
   priority: z.nativeEnum(Priority, { message: "Prioridade é obrigatória" }),
 })
 
-export type CreateTaskSchema = z.infer<typeof createTaskSchema>
+export type TaskSchema = z.infer<typeof taskSchema>
