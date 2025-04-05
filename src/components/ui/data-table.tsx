e7975d6451b89aa0b34a0b2f9cf8ebf55fce31fc
Adeonir Kohl <adeonir@gmail.com>
2025-04-05 08:47:20 -0300

    Add paddings to table-head and table-cell

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -25,7 +25,7 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
               {headerGroup.headers.map((header) => {
                 return (
                   <TableHead
-                    className="first-of-type:rounded-tl-md last-of-type:rounded-tr-md"
+                    className="first-of-type:rounded-tl-md first-of-type:pl-4 last-of-type:rounded-tr-md last-of-type:pr-2"
                     key={header.id}
                     style={{
                       width: header.getSize() === 150 ? "auto" : header.getSize(),
@@ -43,13 +43,15 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
             table.getRowModel().rows.map((row) => (
               <TableRow className="border-b-gray-300" data-state={row.getIsSelected() && "selected"} key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                  <TableCell className="first-of-type:pl-4 last-of-type:pr-2" key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
                 ))}
               </TableRow>
             ))
           ) : (
             <TableRow className="border-b-gray-300">
-              <TableCell className="h-24 text-center" colSpan={columns.length}>
+              <TableCell className="h-24 text-center first-of-type:pl-4 last-of-type:pr-2" colSpan={columns.length}>
                 Sem resultados!
               </TableCell>
             </TableRow>
